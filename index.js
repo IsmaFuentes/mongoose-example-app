3275505a61c12026db98cb32fd4548e7b158fd29
Ismael Fuentes <ismafuentes@hotmail.es>
2023-02-23 20:07:29 +0100

    create employee

--- index.js
+++ index.js
@@ -31,30 +31,53 @@ const startApp = async () => {
   while (true) {
     console.log('\n1 - Mostrar lista de vehículos');
     console.log('2 - Mostrar lista de empleados');
-    console.log('3 - Mostrar lista de productos');
-    console.log('4 - Añadir una revisión');
-    console.log('5 - Crear una factura');
+    console.log('3 - Dar de alta un empleado');
+    console.log('4 - Mostrar lista de productos');
+    console.log('5 - Añadir una revisión');
+    console.log('6 - Crear una factura');
     console.log('0 - Salir\n');
 
     const n = Number(prompt('¿que acción quieres realizar?: '));
 
     if (!isNaN(n)) {
       if (n === 1) {
+        // mostrar lista de vehículos
         let data = await mongoose.model('Vehicle').find({}).populate('revisions.employee');
         console.log(JSON.stringify(data, null, 2));
       }
 
       if (n === 2) {
+        // mostrar lista de empleados
         let data = await mongoose.model('Employee').find({});
         console.log(JSON.stringify(data, null, 2));
       }
 
       if (n === 3) {
+        // dar de alta empleados
+        let name = prompt('Nombre: ');
+        let surnames = prompt('Apellidos: ');
+        let dni = prompt('DNI: ');
+        let date = prompt('Fecha nacimiento: ');
+
+        if (isNaN(Date.parse(date)) === false) {
+          await mongoose
+            .model('Employee')
+            .create({ name, surnames, identificationNumber: dni, birthdate: date })
+            .then((emp) => console.log(JSON.stringify(emp, null, 2)))
+            .catch((err) => console.log(err));
+        } else {
+          console.log('¡Fecha inválida!');
+        }
+      }
+
+      if (n === 4) {
+        // mostrar lista de productos
         let data = await mongoose.model('Product').find({});
         console.log(JSON.stringify(data, null, 2));
       }
 
-      if (n === 4) {
+      if (n === 5) {
+        // añadir revisión a un vehículo
         let vehicleId = prompt('Id del vehículo: ');
         let employeeId = prompt('Id del encargado: ');
         let kms = Number(prompt('Kilometros del coche: '));
@@ -73,8 +96,8 @@ const startApp = async () => {
           .then((updated) => console.log(JSON.stringify(updated, null, 2)));
       }
 
-      if (n === 5) {
-        // factura autogenerada a partir de la lista de productos
+      if (n === 6) {
+        // generar factura (autogenerada a partir de la lista de productos)
         const products = await mongoose.model('Product').find({}).lean();
         const baseAmount = products.reduce((acc, prod) => acc + prod.price, 0);
         const totalAmount = baseAmount * 1.21;
@@ -103,6 +126,7 @@ const startApp = async () => {
       }
 
       if (n === 0) {
+        // salir
         process.exit(0);
       }
     }
